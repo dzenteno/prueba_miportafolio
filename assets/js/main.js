feaa58b2fb5fac709a3b85b7bc92dde54559b961
diegozenteno <d.zenteno01@ufromail.cl>
2021-07-01 04:13:47 +0000

    underline y otros detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-
+    // Tooltips en iconos
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
       })
-      
+
     $("a").click(function (event) {
         event.preventDefault();
 
-        // SmoothScroll variable gato guarda los id
+        // SmoothScroll - variable gato guarda los id
         let gato = this.hash;
         $("html").animate({
             scrollTop: $(gato).offset().top,
